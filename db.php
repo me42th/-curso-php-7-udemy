57a40059b90f67d40c781cd4dddcca588645a3f6
David Meth <david.meth@temet.com.br>
2018-12-05 19:29:58 -0300

    mysqli

--- db.php
+++ db.php
@@ -4,6 +4,10 @@
    // var_dump($con);
     if($con->connect_error)
         echo $con->connect_error;
-        
+    $stmt = $con->prepare("INSERT INTO transac values(default,?,?,null,null,null,null)");    
+    $stmt->bind_param("si",$desc,$enum);
+    $desc = "texto";
+    $enum = 1;    
+    $stmt->execute();
 
 ?>
