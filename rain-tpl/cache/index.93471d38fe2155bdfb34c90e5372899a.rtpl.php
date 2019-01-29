79a2348459b38f439b95ca0fc4c2c7b0a7209c83
David Meth <david.meth@temet.com.br>
2019-01-29 15:16:32 -0300

    rain-tpl

---
+++ rain-tpl/cache/index.93471d38fe2155bdfb34c90e5372899a.rtpl.php
@@ -0,0 +1,5 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>
+    Hello <?php echo htmlspecialchars( $name, ENT_COMPAT, 'UTF-8', FALSE ); ?>
+
+</h1>
+<p>Versão <?php echo htmlspecialchars( $version, ENT_COMPAT, 'UTF-8', FALSE ); ?></p>
